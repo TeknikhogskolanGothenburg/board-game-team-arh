dd41f109e176ca16a749520143e17e3482ea8108
Rasmus Berg <chibi86@hotmail.com>
2018-01-18 15:41:16 +0100

    Start building game
    Start making classes, variables and methods

---
+++ Scr/BattleShipNet/GameEngine/GameBoard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class GameBoard
+    {
+        public Player[] Players { get; }
+        public string GameKey { get; }
+        public int Turn { get; }
+
+        public GameBoard()
+        {
+        }
+
+        public bool Shoot(Position position)
+        {
+            return false;
+        }
+
+        public bool IsGameEnd(out Player winner)
+        {
+            winner = null;
+            return false;
+        }
+    }
+}
