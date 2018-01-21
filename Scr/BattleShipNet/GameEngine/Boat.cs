bc9b7426ebfa96341e528d67a3c6f48918c68032
Rasmus Berg <chibi86@hotmail.com>
2018-01-21 12:23:13 +0100

    Boat class build and Player class rebuild
    Boat class build from class
    Player class rebuild

--- Scr/BattleShipNet/GameEngine/Boat.cs
+++ Scr/BattleShipNet/GameEngine/Boat.cs
@@ -8,34 +8,104 @@ namespace GameEngine
 {
     public class Boat
     {
-        public Position[] Positions { get;}
+        public Position[] Positions { get; private set; }
         public BoatType Type { get; }
+        public int Size {
+            get {
+                return (int)Type;
+            }
+        }
         public Position[] Hits { get; }
+
         public bool Sink
         {
             get
             {
+                if(Hits.Length == Size)
+                {
+                    return true;
+                }
+
                 return false;
             }
         }
 
+        /// <summary>
+        /// Constructor which takes BoatType
+        /// </summary>
+        /// <param name="newBoatType">Boat type on Boat (BoatType)</param>
         public Boat(BoatType newBoatType)
         {
-
+            Type = newBoatType;
+            Positions = new Position[2];
+            Hits = new Position[Size];
         }
 
-        public void SetPositions(Position[] position)
+        /// <summary>
+        /// Set position for Boat
+        /// </summary>
+        /// <param name="newPositions">Hit position (Position)</param>
+        public void SetPositions(Position[] newPositions)
         {
-
+            // Check so position is not diagonally
+            if (newPositions[0].Y == newPositions[1].Y || newPositions[0].X == newPositions[1].X)
+            {
+                // Check so Boat not position over it's size
+                if (
+                    ((newPositions[1].Y - newPositions[0].Y) == Size || (newPositions[0].Y - newPositions[1].Y) == Size) &&
+                    ((newPositions[1].X - newPositions[0].X) == Size || (newPositions[0].X - newPositions[1].X) == Size)
+                )
+                {
+                    // Position[1] should be largest, if it's not switch
+                    if(newPositions[1].Y > newPositions[0].Y || newPositions[1].X > newPositions[0].X)
+                    {
+                        Positions = newPositions;
+                    }
+                    else
+                    {
+                        Positions[0] = newPositions[1];
+                        Positions[1] = newPositions[0];
+                    }
+                }
+                else
+                {
+                    throw new FormatException("Boat cannot be position over it's size");
+                }
+            }
+            else
+            {
+                throw new FormatException("Boat cannot be position diagonally");
+            }
         }
 
+        /// <summary>
+        /// If Boat is here, return true
+        /// </summary>
+        /// <param name="position">Position to check (Position)</param>
+        /// <returns>Validate result (bool)</returns>
         public bool AreYouHere(Position position)
         {
+            if(position.X >= Positions[0].X && position.X <= Positions[1].X && position.Y >= Positions[0].Y && position.Y <= Positions[1].Y)
+            {
+                return true;
+            }
+
             return false;
         }
 
+        /// <summary>
+        /// If Boat is hit, return true and add hit to Boat
+        /// </summary>
+        /// <param name="position">Hit position (Position)</param>
+        /// <returns>Validate result (bool)</returns>
         public bool IsItAHit(Position position)
         {
+            if (AreYouHere(position))
+            {
+                Hits[Hits.Length] = position;
+                return true;
+            }
+
             return false;
         }
     }
--- Scr/BattleShipNet/GameEngine/Player.cs
+++ Scr/BattleShipNet/GameEngine/Player.cs
@@ -9,66 +9,88 @@ namespace GameEngine
     public class Player
     {
         public string Name { get; set; }
-        public Boat[] Boats { get;set; }
-        //public bool[] Trys { get; set;}  
-        public List<Position> AlreadyHitPositions{get;set;}
-            
+        public Boat[] Boats{ get; }
+        public List<Position> AlreadyHitPositions{ get; private set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Player()
         {
             AlreadyHitPositions = new List<Position>();
-            Boats = new Boat[10];
+
+            Boats = new Boat[10] {
+                new Boat(BoatType.Battleship),
+                new Boat(BoatType.Cruiser),
+                new Boat(BoatType.Cruiser),
+                new Boat(BoatType.Destroyer),
+                new Boat(BoatType.Destroyer),
+                new Boat(BoatType.Destroyer),
+                new Boat(BoatType.Submarine),
+                new Boat(BoatType.Submarine),
+                new Boat(BoatType.Submarine),
+                new Boat(BoatType.Submarine)
+            };
         }
 
-        public void LoadBoats(Boat[] boats)
+        /// <summary>
+        /// Is position already hit
+        /// </summary>
+        /// <param name="position">Position to hit (Position)</param>
+        /// <returns>Validate result (bool)</returns>
+        public bool IsPositionAlreadyHit(Position position)
         {
-            Boats=boats;
-        }
-        
-        public bool IsTryAlreadyDone(Position position)
-        {
-
-            if (AlreadyHitPositions.Contains(position))
-            {
+             if(AlreadyHitPositions.Contains(position))
+             {
                 return true;
-            }
-            return false;
+             }
+
+             return false;
         }
 
-        public bool IsItAHit(Position position)
+        /// <summary>
+        /// Check if any Boat was hit
+        /// </summary>
+        /// <param name="position">Position to hit (Position)</param>
+        /// <returns>Validate result (bool)</returns>
+        public bool IsABoatHit(Position position)
         {
-            if(!IsTryAlreadyDone(position))
+            if(IsPositionAlreadyHit(position))
             {
+                throw new Exception("Position is already hit, try other position!");
+            }
+            else
+            {
+                AlreadyHitPositions.Add(position);
+
                 foreach (var boat in Boats)
                 {
-                    foreach (var pos in boat.Positions)
+                    if (boat.IsItAHit(position))
                     {
-                        if (position.X == pos.X && position.Y == pos.Y)
-                        {
-                            AlreadyHitPositions.Add(position);
-                            return true;
-                        }
+                        return true;
                     }
-
                 }
-            }           
+            }
+            
+
             return false;
         }
 
+        /// <summary>
+        /// Check if player not got any Boats left, if not player has lost
+        /// </summary>
+        /// <returns>Validate result (bool)</returns>
         public bool HasPlayerLost()
         {
             foreach(var boat in Boats)
             {
-               if(!boat.Sink)
+                if(!boat.Sink)
                 {
                     return false;
                 }                            
             }
+
             return true;
         }
-
-
-
     }
-
-    
 }
