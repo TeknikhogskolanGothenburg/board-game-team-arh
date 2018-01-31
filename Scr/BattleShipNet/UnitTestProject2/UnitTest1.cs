d504410c3dfc4c68647d7d0d2824d3493e52ad39
Adrianaxj <35222944+Adrianaxj@users.noreply.github.com>
2018-01-31 22:31:52 +0100

    unittest

---
+++ Scr/BattleShipNet/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { }
+            [TestMethod]
+        public void IsPositionAlreadyHitShoot()
+        {
+            // Arrange
+            GameBoard gameBoard = new GameBoard();
+            Player player = gameBoard.Players[0];
+            Position pos = new Position(1, 2);
+            player.AlreadyHitPositions.Add(pos);
+            Position pos1 = new Position(1, 2);
+
+            player.IsPositionAlreadyHit(pos1);
+            // Act
+            var result = player.IsPositionAlreadyHit(pos1);
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void IsAnyBoatHere()
+        {
+            // Arrange   
+            GameBoard gameBoard = new GameBoard();
+            Player player1 = gameBoard.Players[0];
+            Boat boatCruiser = new Boat(BoatType.Cruiser);
+            Position[] positions = new Position[2] {
+                        new Position(1, 2),
+                        new Position(5, 2)
+                    };
+
+            player1.IsAnyBoatHere(new Position(1, 2));
+
+            // Act
+            var result = player1.IsAnyBoatHere(new Position(1, 2));
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+    }
+}
